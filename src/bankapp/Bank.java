df016451c5f4bc4bc82713e8f2375fdd85ad0da7
Mustafa Shubbar <codingbox@outlook.com>
2021-01-10 18:38:38 -0500

    fixing private field member issue

--- src/bankapp/Bank.java
+++ src/bankapp/Bank.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Bank {
 
     private String name;
-    ArrayList<Branch> branches;
+    private ArrayList<Branch> branches;
 
     public Bank(String name) {
         this.name = name;
@@ -52,27 +52,27 @@ public class Bank {
         return null;
     }
 
-    public boolean listCustomers(String branchName, boolean showTransactions) {
-        Branch branch = findBranch(branchName);
-        if (branch != null) {
-            System.out.println("Customers for branch " + branch.getName());
-            ArrayList<Customer> branchCustomers = branch.getCustomers();
-            for (int i = 0; i < branchCustomers.size(); i++) {
-                Customer branchCustomer = branchCustomers.get(i);
-                System.out.println("Customer " + branchCustomer.getName() + "[" + (i + 1) + "]");
-                if (showTransactions) {
-                    System.out.println("Transactions:");
-                    ArrayList<Double> transactions = branchCustomer.getTransactions();
-                    for (int j = 0; j < transactions.size(); j++) {
-                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
-                    }
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
+   public boolean listCustomers(String branchName, boolean showTransactions) {
+       Branch branch = findBranch(branchName);
+       if(branch != null) {
+           System.out.println("Customer details for branch " + branch.getName());
 
-    }
+           ArrayList<Customer> branchCustomers = branch.getCustomers();
+           for(int i=0; i<branchCustomers.size(); i++) {
+               Customer branchCustomer = branchCustomers.get(i);
+               System.out.println("Customer: " + branchCustomer.getName() + "[" + (i+1) + "]");
+               if(showTransactions) {
+                   System.out.println("Transactions");
+                   ArrayList<Double> transactions = branchCustomer.getTransactions();
+                   for(int j=0; j<transactions.size(); j++) {
+                       System.out.println("[" + (j+1) + "]  Amount "  + transactions.get(j));
+                   }
+               }
+           }
+           return true;
+       } else {
+           return false;
+       }
+   }
 
 }
